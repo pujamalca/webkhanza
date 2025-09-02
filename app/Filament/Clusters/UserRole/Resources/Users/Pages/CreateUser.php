b1732d4bc72dea2f2d27fe5bc6ce3fd23da020e1
pujamalca <pujamalca09@gmail.com>
2025-09-02 15:08:28 +0700

    fixed : id user

--- app/Filament/Clusters/UserRole/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Clusters/UserRole/Resources/Users/Pages/CreateUser.php
@@ -17,7 +17,32 @@ class CreateUser extends CreateRecord
         // Remove pegawai_nik from data as it's not a user field
         unset($data['pegawai_nik']);
         
+        // Generate explicit ID for varchar(20) field
+        $data['id'] = $this->generateUniqueUserId();
+        
         return $data;
+    }
+    
+    private function generateUniqueUserId(): string
+    {
+        // Get the last numeric ID from existing users
+        $lastUser = \App\Models\User::whereRaw('id REGEXP "^[0-9]+$"')
+            ->orderByRaw('CAST(id AS UNSIGNED) DESC')
+            ->first();
+            
+        if ($lastUser && is_numeric($lastUser->id)) {
+            $nextId = (string)((int)$lastUser->id + 1);
+        } else {
+            // If no numeric IDs exist, start from 2 (since admin is likely ID 1)
+            $nextId = '2';
+        }
+        
+        // Make sure the ID doesn't already exist (safety check)
+        while (\App\Models\User::where('id', $nextId)->exists()) {
+            $nextId = (string)((int)$nextId + 1);
+        }
+        
+        return $nextId;
     }
     
     protected function afterCreate(): void
--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,19 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles, LogsActivity;
 
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
 
     /**
      * The attributes that are mass assignable.
@@ -26,7 +39,9 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
      * @var list<string>
      */
     protected $fillable = [
+        'id',
         'name',
+        'username', 
         'email',
         'password',
         'avatar_url',
